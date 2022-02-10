1c197405398ec72dfec294bbfea33068c2021ac9
Jacira Ramos <99293415+Jacira-Ramos@users.noreply.github.com>
2022-02-10 10:09:35 +0100

    Update function_multiplesfiles.r

--- function_multiplesfiles.r
+++ function_multiplesfiles.r
@@ -16,3 +16,24 @@ my_nextlevel_function <- function(filename,fileformat){
 #View(aa)
 #bb <- my_nextlevel_function("Table_2.tsv","tsv")
 #View(bb)
+###################################################################
+complicated_function<-function(filename,fileformat){
+  if(fileformat=="csv"){
+    importedfile<-read.csv(filename,header=t,sep=";")
+    }
+  else if(fileformat=="tsv"){
+    importedfile<-read.delim(filename,header=T,sep="\t")
+    }
+    else if(fileformat=="txt"){
+      importedfile<-read.delim2(filename,header=T,sep="\t",dec=".")
+      }
+      else(fileformat=="url"){
+        url<-"https://pubchem.ncbi.nlm.nih.gov/sdq/sdqagent.cgi?infmt=json&outfmt=csv&query=
+              {%22download%22:%22*%22,%22collection%22:%22bioactivity%22,%22where%22:{%22ands%22:[{%22cid%22:%2224764%22}]},
+              %22order%22:[%22acvalue,asc%22],%22start%22:1,%22limit%22:10000000,%22downloadfilename%22:%22CID_24764_bioactivity%22,
+              %22nullatbottom%22:1}"
+       importedfile <- "C:/Users/JaciraRamos/Desktop/output.csv" #review this part, i think smth is not correct
+       download.file(url,importedfile)
+       }
+       return(importedfile)
+      }
