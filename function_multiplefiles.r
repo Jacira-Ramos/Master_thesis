7ebd3412072f41df893504b036fbcc320631e346
Jacira Ramos <99293415+Jacira-Ramos@users.noreply.github.com>
2022-02-14 12:52:17 +0100

    Update function_multiplefiles.r

--- function_multiplefiles.r
+++ function_multiplefiles.r
@@ -20,7 +20,7 @@ my_nextlevel_function <- function(filename,fileformat){
 ###################################################################
 complicated_function<-function(filename,fileformat){
   if(fileformat=="csv"){
-    importedfile<-read.csv(filename,header=t,sep=";")
+    importedfile<-read.csv(filename,header=T,sep=";")
     }
   else if(fileformat=="tsv"){
     importedfile<-read.delim(filename,header=T,sep="\t")
@@ -28,12 +28,13 @@ complicated_function<-function(filename,fileformat){
     else if(fileformat=="txt"){
       importedfile<-read.delim2(filename,header=T,sep="\t",dec=".")
       }
-      else(fileformat=="url"){
-        url<-"https://pubchem.ncbi.nlm.nih.gov/sdq/sdqagent.cgi?infmt=json&outfmt=csv&query=
-              {%22download%22:%22*%22,%22collection%22:%22bioactivity%22,%22where%22:{%22ands%22:[{%22cid%22:%2224764%22}]},
-              %22order%22:[%22acvalue,asc%22],%22start%22:1,%22limit%22:10000000,%22downloadfilename%22:%22CID_24764_bioactivity%22,
-              %22nullatbottom%22:1}"
-       importedfile <- "C:/Users/JaciraRamos/Desktop/output.csv" #review this part, i think smth is not correct
+        else if(filefromat=="xlsx"){
+      importedfile<- read_excel(filename,header=T)
+      }
+      else {fileformat=="url"
+        url<-"filename"}
+       importedfile <- getwd()
+      #"C:/Users/JaciraRamos/Desktop/output.csv" #review this part, i think smth is not correct
        download.file(url,importedfile)
        }
        return(importedfile)
