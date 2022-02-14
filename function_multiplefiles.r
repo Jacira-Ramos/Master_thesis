a7d5baa5223f4d10435de26a8a4e0a1ca76057ba
Jacira Ramos <99293415+Jacira-Ramos@users.noreply.github.com>
2022-02-14 13:53:05 +0100

    Update function_multiplefiles.r

--- function_multiplefiles.r
+++ function_multiplefiles.r
@@ -1,23 +1,3 @@
-##Example
-my_nextlevel_function <- function(filename,fileformat){
-  if(fileformat=="csv"){
-    fileoutput <- read.csv(filename, header = T,sep = ";")
-  }
-  else if(fileformat=="tsv"){
-    fileoutput <- read.delim(filename, header=T, sep = "\t")
-  }
-  else{
-    print("The given file format is not acceptable")
-    fileoutput <- "NA"
-  }
-  return(fileoutput)
-}
-
-# aa <- my_nextlevel_function("Table_4.csv","csv")
-#View(aa)
-#bb <- my_nextlevel_function("Table_2.tsv","tsv")
-#View(bb)
-###################################################################
 complicated_function<-function(filename,fileformat){
   if(fileformat=="csv"){
     importedfile<-read.csv(filename,header=T,sep=";")
