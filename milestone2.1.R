8d3feea613e40359366bb4a6c2ee6afbf800bff0
Jacira Ramos <99293415+Jacira-Ramos@users.noreply.github.com>
2022-03-13 22:58:48 +0100

    Add files via upload
    Sorted distances

---
+++ milestone2.1.R
@@ -0,0 +1,23 @@
+#Create empty result table of dimension 29 by 2
+#Using “For loop” extract each row of distance metrics, convert the row values to numeric
+#each_row = as.numeric(TABLE_7[1,])
+#temp_results = sort(each_row, index.return=TRUE)
+
+empty_table <- matrix(nrow = nrow(data),ncol=2)
+
+for (i in 1:nrow(data)){
+  extract <-as.numeric(data[1,])
+  sorted <- sort(extract, index.return=TRUE)
+  print(sorted)
+ }
+ 
+for (i in 1:nrow(euclidean_distance_table)){
+  each_row <-as.numeric(euclidean_distance_table[1,])
+  temp_results <- sort(extract, index.return=TRUE)
+  print(temp_results)
+}
+
+sorted_distance <- matrix(ncol = 2,nrow = nrow(euclidean_distance_table))
+
+sorted_distance[,2] <- test1_results$x
+sorted_distance[,1] <- test1_results$ix
